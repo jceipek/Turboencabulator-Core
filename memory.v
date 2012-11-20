897ee3342f96503446f202c68ef4b32e77393132
Julian Ceipek <julian.ceipek@gmail.com>
2012-11-20 17:34:24 -0500

    Made the memory.v file load in the add.dat file and display it.

--- memory.v
+++ memory.v
@@ -4,21 +4,31 @@ part of project Turboencabulator
 Julian Ceipek, Yuxin Guan, Philip Z Loh, Sasha Sproch
 Computer Architecture, Olin College Fall 2012 */
 
-module memory(clk, regWE, Addr, 
+module memory(clk, regWE, Addr,
                 DataIn, DataOut);
   input clk, regWE;
   input[9:0] Addr;
   input[31:0] DataIn;
   output[31:0]  DataOut;
-  
-reg [31:0] mem[1023:0];  
+
+  integer index;
+
+reg [31:0] mem[1023:0];
 
 always @(posedge clk)
   if (regWE)
     mem[Addr] <= DataIn;
 
-initial $readmemb(?file.dat?, mem);
-    
+initial begin
+    // Read the code into memory
+    $readmemb("add.dat", mem);
+
+    // Loop through every row of memory and display it
+    for(index = 0; index < 1024; index = index + 1) begin
+        $display("mem[%d] = %b", index[9:0], mem[index]);
+    end
+end
+
 assign DataOut = registers[Addr];
 
 endmodule
